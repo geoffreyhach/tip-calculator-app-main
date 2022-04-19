35059b6b2f2939cd6aaad8bcb0ef18cd0c97d8bb
geoffjecrois <je.suis.geoffrey.hach@gmail.com>
2022-04-19 15:10:42 +0200

    oui

---
+++ main.js
@@ -0,0 +1,90 @@
+
+let tipBtns = document.querySelectorAll('.tip');
+let customTipBtn = document.querySelector('.custom-tip');
+let billInput = document.getElementById('bill');
+let peopleNumberInput = document.getElementById('people');
+let totalPerPerson = document.querySelector('.total-per-person-result');
+let tipPerPerson = document.querySelector('.tip-per-person-result');
+let error = document.querySelector('.error');
+let resetBtn = document.querySelector('.reset');
+
+
+billInput.value = 0.0
+peopleNumberInput.value = 1;
+
+let billValue = 0.0;
+let peopleNumber = 1;
+let tip = 1;
+window.onload = calculate();
+
+tipBtns.forEach(el => {
+    el.addEventListener('click', () => {
+        
+        customTipBtn.classList.remove('active-custom');
+        tipBtns.forEach(el => el.classList.remove('active'))
+        el.classList.add('active')
+    });
+});
+
+customTipBtn.addEventListener('click', () => {
+    tipBtns.forEach(el => el.classList.remove('active'));
+    customTipBtn.value="";
+    customTipBtn.classList.add('active-custom');
+    
+});
+
+billInput.addEventListener('input', () => {
+    billValue = billInput.value;
+    calculate()
+});
+
+
+peopleNumberInput.addEventListener('input', () => {
+    peopleNumber = peopleNumberInput.value;
+    if (peopleNumber < 1)  {
+        error.style.display = "block";
+        peopleNumberInput.style.outline = "2px solid red";
+    }
+    else {
+        error.style.display = "none";
+        peopleNumberInput.style.outline = "none";
+        calculate()
+    }
+});
+
+tipBtns.forEach(el => {
+    el.addEventListener('click', () => {
+        tip = (1 + el.value/100);
+        calculate();
+    })
+
+})
+
+customTipBtn.addEventListener('input', () => {
+    tip = (1 + customTipBtn.value/100);
+    console.log(tip);
+    calculate()
+});
+
+function calculate() {
+    let tipTotal = ((tip - 1) * billValue) / peopleNumber
+    tipPerPerson.innerHTML = tipTotal.toFixed(2);
+    let total = (billValue * tip) / peopleNumber ;
+    totalPerPerson.innerHTML = total.toFixed(2);
+}
+
+resetBtn.addEventListener('click', () => {
+    billInput.value = 0.0;
+    peopleNumberInput.value = 1
+    tip = 1;
+    billValue = 0.0;
+    peopleNumber = 1;
+    customTipBtn.value=""
+    
+    tipBtns.forEach(el => el.classList.remove('active'));
+    calculate();
+
+})
+
+
+
